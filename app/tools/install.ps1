1e9c2eb7f442a5fee8103a2b6657add70e4bf0d6
YEVHEN POTAPOV <yevhen.y.p.u.potapov@gmail.com>
2026-09-17 11:39:46 -0700

    Add copyright header: app/tools/install.ps1

--- app/tools/install.ps1
+++ app/tools/install.ps1
@@ -1,3 +1,5 @@
+# Copyright (c) 2026 Yevhen Potapov. All rights reserved.
+# Part of ContentForge. See LICENSE in the repository root.
 # ContentForge - install / update Python packages the factory needs.
 $ErrorActionPreference = 'Continue'
 
