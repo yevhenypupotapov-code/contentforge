aed22a0dce7174a9ed174900f1701bd8de28535c
YEVHEN POTAPOV <yevhen.y.p.u.potapov@gmail.com>
2026-09-17 11:40:02 -0700

    Add copyright header: src/pages/Channels.tsx

--- src/pages/Channels.tsx
+++ src/pages/Channels.tsx
@@ -1,3 +1,4 @@
+// Copyright (c) 2026 Yevhen Potapov. All rights reserved.
 import { useEffect, useState } from 'react'
 import { api, type Channel } from '../api'
 import { track } from '../analytics'
